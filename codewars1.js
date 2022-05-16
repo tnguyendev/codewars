2b272ada4cb265f2a8074393926740f18893082f
Thao Nguyen <102040536+tnguyendev@users.noreply.github.com>
2022-05-15 22:18:38 -0400

    code wars 1

---
+++ codewars1.js
@@ -0,0 +1,8 @@
+//Simple, remove the spaces from the string, then return the resultant string.
+
+//question
+function noSpace(x){
+  }
+
+  //solution
+function noSpace(x){return x.split(' ').join('')}
