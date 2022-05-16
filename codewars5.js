16b30bcb4c14b015d3dd80b35c372a61c3201642
Thao Nguyen <102040536+tnguyendev@users.noreply.github.com>
2022-05-16 00:12:17 -0400

    code wars 5

---
+++ codewars5.js
@@ -0,0 +1,16 @@
+//Implement a function which convert the given boolean value into its string representation.
+
+//question
+function booleanToString(b){
+    //your code here
+  }
+
+
+//solution
+
+function booleanToString(b){
+    return String(b);
+  }
+
+///I FUCKING GOT THIS ONE LETS FUCKING GOOOOOOO I DID IT
+
