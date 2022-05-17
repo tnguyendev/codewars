ae71d8d2265d87e42928fa3c6df1e5af51ce2a8e
Thao Nguyen <102040536+tnguyendev@users.noreply.github.com>
2022-05-16 23:49:25 -0400

    code wars 10

---
+++ codewars10.js
@@ -0,0 +1,83 @@
+//Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+//Please use the following function names:
+
+//addition = add
+
+//multiply = multiply
+
+//division = divide (both integer and float divisions are accepted)
+
+//modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+// Note: All math operations will be: a (operation) b
+
+//question
+function add(a,b){
+    return 
+}
+
+function divide(a,b){
+    return 
+}
+
+function multiply(a,b){
+    return 
+}
+
+function mod(a,b){
+    return 
+}
+   
+function exponent(a,b){
+    return 
+}
+    
+function subt(a,b){
+    return 
+}
+
+
+//solution
+const add = (a, b) => a + b;
+const subt = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+
+//or
+function add(a,b){
+    return a + b
+}
+
+function divide(a,b){
+    return a / b
+}
+
+function multiply(a,b){
+    return a * b
+}
+
+function mod(a,b){
+    return a % b
+}
+   
+function exponent(a,b){
+    return a ** b
+}
+    
+function subt(a,b){
+    return a - b
+}
+
+
+//or
+
+
+
+//or
