9dacdd2705308fd39f55d51eada38b6eb64001c6
Thao Nguyen <102040536+tnguyendev@users.noreply.github.com>
2022-05-17 22:50:06 -0400

    code wars 12

---
+++ codewars12.js
@@ -0,0 +1,33 @@
+//You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+//question
+function positiveSum(arr) {
+  
+}
+
+
+//solution
+function positiveSum(arr) {
+    var total = 0;    
+    for (i = 0; i < arr.length; i++) {    // setup loop to go through array of given length
+      if (arr[i] > 0) {                   // if arr[i] is greater than zero
+        total += arr[i];                  // add arr[i] to total
+      }
+    }
+    return total;                         // return total
+  }
+
+
+//or
+function positiveSum(arr) {
+    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
+ }
+
+//or
+const positiveSum = (arr) => arr.reduce((sum, n) => n > 0 ? sum + n : sum, 0);
+
+//or
