dacfe550bf7a0b9b585645a2bafa6b52b5a25119
Thao Nguyen <102040536+tnguyendev@users.noreply.github.com>
2022-05-23 22:45:07 -0400

    code wars 17

---
+++ codewars17.js
@@ -0,0 +1,16 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+// 1: -1
+// 14: -14
+// -34: 34
+
+//question
+function opposite(number) {
+    //your code here
+  }
+
+
+//solution
+function opposite(number) {
+    return -(number)
+  }
+
