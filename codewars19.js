168cedfac4640806b251bbe5ec9ef2a615184303
Thao Nguyen <102040536+tnguyendev@users.noreply.github.com>
2022-05-27 17:00:46 -0400

    code wars 19

---
+++ codewars19.js
@@ -0,0 +1,11 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//question
+function doubleInteger(i) {
+    return i;
+}
+
+//solution:
+function doubleInteger(i) {
+    return i+i;
+  }
